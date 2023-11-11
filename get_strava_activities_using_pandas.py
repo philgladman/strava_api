7006bc13e236a116c767c5a4a9b966e2c4ebf3e2
Phillip Gladman <Philgladman@Phillps-MacBook-Pro.local>
2023-11-11 14:53:51 -0500

    remove old variable

--- get_strava_activities_using_pandas.py
+++ get_strava_activities_using_pandas.py
@@ -13,7 +13,6 @@ from dotenv import load_dotenv
 load_dotenv()
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.environ.get('CLIENT_SECRET')
-token_from_url = os.environ.get('TOKEN_FROM_URL')
 redirect_url = "https://localhost/exchange_token"
 strava_tokens_outfile = ".strava_tokens.json"
 strava_activities_outfile = "strava_activities.csv"
